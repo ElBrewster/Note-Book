eb9ff2b3c3e2025d259d3f5870ba6e6e23babc69
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-05-16 12:07:54 -0700

    Add loader function to display seed data and read route

--- app/routes/read._index.tsx
+++ app/routes/read._index.tsx
@@ -1,5 +1,24 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+export const loader = async () => {
+    return json({
+        notes: await db.note.findMany(),
+    });
+};
+
 export default function ReadIndex() {
+    const data = useLoaderData<typeof loader>();
     return (
-        <div>read index</div>
+        <div>
+            <h2>read._index</h2>
+            {data.notes.map(note => (
+                <section>
+                    <h3>{note.title}</h3>
+                    <p>{note.body}</p>
+                </section>
+            ))}
+        </div>
     );
 }
