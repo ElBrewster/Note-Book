dc3feb2903a516b2f0cfb67ddc1abf6f9c51a827
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-05-20 10:49:14 -0700

    Add notes for project planning

--- app/routes/note-taking.tsx
+++ app/routes/note-taking.tsx
@@ -1,3 +1,5 @@
+//note-taking route module layout
+
 import { Outlet } from "@remix-run/react";
 import { LinksFunction } from "@remix-run/node";
 import stylesHref from "app/styles/note-taking.css";
@@ -6,20 +8,20 @@ export const links: LinksFunction = () => {
   return [
     {
       rel: "stylesheet",
-      href: stylesHref
-    }
-  ]
-}
+      href: stylesHref,
+    },
+  ];
+};
 
 export default function NoteTaking() {
-    return (
-        <div className="bodyContentWrapper">
-            <header></header>
-            <h1>Take A Note If You Want</h1>
-            <main>
-                <Outlet />
-            </main>
-            <footer>.</footer>
-        </div>
-    );
+  return (
+    <div className="bodyContentWrapper">
+      <header></header>
+      <h1>Take A Note If You Want</h1>
+      <main>
+        <Outlet />
+      </main>
+      <footer>.</footer>
+    </div>
+  );
 }
--- research.md
+++ research.md
@@ -6,3 +6,4 @@ List of Helpful Resources
 - [see Form hook APIs](https://betterprogramming.pub/mastering-remix-forms-615cab9a274d)
 - [conversation on useState with Remix.js](https://stackoverflow.com/questions/71005650/handling-ui-state-in-remix-run)
 - [remix-utils for client only](https://github.com/sergiodxa/remix-utils#clientonly)[and](https://github.com/sergiodxa/remix-utils#usehydrated)
+- [getting data from parent route to the child](https://spin.atomicobject.com/2023/01/26/remix-routing/)
