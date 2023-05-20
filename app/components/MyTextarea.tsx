0e2c865fe1722490982b39dacf8d2b7f67c5cc10
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-05-20 16:33:35 -0700

    Add Rich text area implementation to text area component

--- app/components/MyTextarea.tsx
+++ app/components/MyTextarea.tsx
@@ -1,4 +1,6 @@
 import { Controller, useForm } from "react-hook-form";
+import { RichTextarea } from "rich-textarea";
+import Highlighter from "react-highlight-words";
 
 export default function MyTextarea() {
   const {
@@ -7,21 +9,45 @@ export default function MyTextarea() {
     watch,
     control,
     formState: { errors },
-  } = useForm({});
+  } = useForm({
+    defaultValues: {textarea: "",}
+  });
+
+  const style = { padding: ".5rem"};
 
   return (
     <div className="textareaWrapper">
         <form onSubmit={handleSubmit((data) => {console.log(data)})}>
+
             <label htmlFor="textarea">Add your notes here:</label>
+
             <Controller
             control={control}
             name="textarea"
             rules={{required: true}}
             render={({
-                field: {}
+                field: { onChange, onBlur, value, name, ref}
             }) => (
-                <p></p>
-            )}
+                <RichTextarea 
+                    aria-invalid={errors.textarea ? "true" : "false"}
+                    value={value}
+                    style={style}
+                    placeholder="notes"
+                    name={name}
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    ref={ref}
+                >
+                    {(v) => {
+                    return v.split("").map((t, i) => (
+                        <span key={i} style={{ color: i % 2 === 0 ? "red" : undefined }}>
+                        {t}
+                        </span>
+                    ));
+                }}
+                </RichTextarea>)
+                    
+                }
             />
             <input type="submit" />
         </form>
